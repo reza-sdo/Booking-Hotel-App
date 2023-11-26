a74066588a22c931bcdf98ed47f3e0d2e92f2963
Reza <rezammdzd0@gmail.com>
2023-11-26 19:00:36 +0330

    create header UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,38 @@
 import { MdLocationOn } from "react-icons/md";
+import { HiCalendar, HiSearch } from "react-icons/hi";
+import { useState } from "react";
 
 function Header() {
+  const [destination , setDestination ] = useState("")
   return (
     <div className="header">
       <div className="headerSearch">
         <div className="headerSearchItem">
-          <MdLocationOn className="headerIcon" />
+          <MdLocationOn className="headerIcon locationIcon" />
           <input
             type="text"
             placeholder="where to go?"
             className="headerSearchInput"
             name="destination"
-            
+            value={destination}
+            onChange={e=>setDestination(e.target.value)}
           />
+          <span className="seperator"></span>
         </div>
-        <div className="headerSearchItem"></div>
-        <div className="headerSearchItem"></div>
-        <div className="headerSearchItem"></div>
+        <div className="headerSearchItem">
+          <HiCalendar className="headerIcon dateIcon" />
+          <div className="dateDropDown">2023/06/23</div>
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <div id="optionDropDown">1 adult &bull; 2children &bull; 1room</div>
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <button className="headerSearchBtn">
+            <HiSearch className="headerIcon" />
+          </button>
+        </div>
       </div>
     </div>
   );
